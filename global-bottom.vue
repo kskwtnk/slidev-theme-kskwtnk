284704553cbde5ff8372ee3bb6cf6f4f71b4b82d
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2026-02-01 22:29:57 +0900

    Add global style

---
+++ global-bottom.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useSlideContext } from '@slidev/client'
+
+const { $slidev } = useSlideContext()
+</script>
+
+<template>
+  <footer class="absolute bottom-4 left-0 right-0 px-6 text-xs flex justify-between text-stone-500">
+    <span>{{ $slidev.themeConfigs.footerLeft }}</span>
+    <span>{{ $slidev.themeConfigs.footerRight }}</span>
+  </footer>
+</template>
