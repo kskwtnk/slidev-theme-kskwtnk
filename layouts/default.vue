29de00ef66ffeb35f618aacd3130ccf03f4e7cf6
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2026-03-06 13:16:56 +0900

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="slidev-layout default">
+    <div class="my-auto w-full">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+:deep(>*+*) {
+  @apply mt-4;
+}
+
+:deep(h1) {
+  @apply text-4xl font-extrabold my-5;
+}
+</style>
