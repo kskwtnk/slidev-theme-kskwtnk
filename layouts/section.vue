c6ed15e138018d4945a656c67c69b0aab06f48e5
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2026-03-06 18:53:23 +0900

    Add section layout

---
+++ layouts/section.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="slidev-layout section flex flex-col justify-center">
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+:deep(> * + *) {
+  --at-apply: mt-4;
+}
+
+:deep(h1) {
+  --at-apply: text-5xl font-extrabold my-5;
+}
+</style>
