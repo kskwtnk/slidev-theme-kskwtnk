9ad1fbab59f45247f85b078642cddffc33831b8e
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2026-03-06 13:12:04 +0900

    Add header

---
+++ global-top.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useSlideContext } from '@slidev/client'
+
+const { $slidev } = useSlideContext()
+</script>
+
+<template>
+  <header class="absolute top-4 left-0 right-0 px-6 text-xs text-stone-500">
+    <span>{{ $slidev.themeConfigs.headerText }}</span>
+  </header>
+</template>
