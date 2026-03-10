292ff8de6afc643ccaa5c54e93ed1071c0931215
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2026-03-10 15:47:38 +0900

    Add center layout

---
+++ layouts/center.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="slidev-layout center grid place-items-center text-4xl">
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+:deep(> * + *) {
+  --at-apply: mt-3;
+}
+
+:deep(h1) {
+  --at-apply: text-6xl font-extrabold my-5;
+}
+</style>
